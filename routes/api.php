e9d833221f8154e8446c29794c1779a08b51ef98
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-29 12:23:39 +0200

    feat filter tickets API

--- routes/api.php
+++ routes/api.php
@@ -16,5 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+  return $request->user();
 });
+
+Route::middleware('auth:sanctum')->get('/tickets', [TicketController::class, 'index']);
