5b77356e7581406f77df398a4f00c12d2c09c021
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-30 22:41:41 +0200

    feat user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,16 +2,22 @@
 
 namespace Database\Seeders;
 
+use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    $user = new User();
+    $user->name = 'admin';
+    $user->email = 'admin@admin.com';
+    $user->password = Hash::make('password');
+    $user->save();
+  }
 }
